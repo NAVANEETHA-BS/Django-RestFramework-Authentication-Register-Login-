a431d4affab2d227739e5dab854f6837263fd9da
NAVANEETHA-BS <navaneetha.bs@gmail.com>
2021-05-26 17:02:54 +0530

    first commit

---
+++ helpers/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+
+class TrackingModel(models.Model):
+    created_at = models.DateTimeField(_('created_at'), auto_now_add=True)
+    updated_at = models.DateTimeField(_('updated_at'), auto_now=True)
+
+    class Meta:
+        abstract=True
+        ordering=('-created_at',)
